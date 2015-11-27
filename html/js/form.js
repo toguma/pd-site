cbc07c5c54b05a5ab37abd58eaec3e6ff75a0e1c
Takeshi Oguma <toguma@proudit.jp>
2015-11-27 18:11:57 +0900

    add lambda form

---
+++ html/js/form.js
@@ -0,0 +1,28 @@
+    <script>
+        var $id = function(id) { return document.getElementById(id); };
+        AWS.config.region = "ap-northeast-1";
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: "ap-northeast-1:7a277149-9693-42d5-af02-75930f859d19"});
+        AWS.config.credentials.get(function(err) {
+            if (!err) {
+                console.log("Cognito Identify Id: " + AWS.config.credentials.identityId);
+            }
+        });
+
+        function uploadFile() {
+            AWS.config.region = 'ap-northeast-1';
+            var s3BucketName = "pd-form-data";
+            var now = new Date();
+            var obj = {"title":$id("title").value, "mail":$id("mail").value ,"contents":$id("contents").value, "date": now.toLocaleString()};
+            var s3 = new AWS.S3({params: {Bucket: s3BucketName}});
+            var blob = new Blob([JSON.stringify(obj, null, 2)], {type:'text/plain'});
+            s3.putObject({Key: "uploads/" +now.getTime()+".txt", ContentType: "text/plain", Body: blob, ACL: "public-read"},
+            function(err, data){
+                if(data !== null){
+                    alert("お問い合わせ完了致しました");
+                }
+                else{
+                    alert("Upload Failed" + err.message);
+                }
+            });
+        }
+    </script>
---
+++ lambda/index.js
@@ -0,0 +1,54 @@
+console.log("Loading event")
+var aws = require('aws-sdk');
+var s3 = new aws.S3({apiVersion: '2006-03-01'});
+var ses = new aws.SES({apiVersion: '2010-12-01',
+                        accessKeyId: 'AKIAIPTDDARU7OYYWJDQ',
+                        secretAccesskey: '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',
+                        region: 'us-east-1' });
+exports.handler = function(event, context) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    var bucket = event.Records[0].s3.bucket.name;
+    var key = event.Records[0].s3.object.key;
+    s3.getObject({Bucket: bucket, Key: key},
+        function(err, data) {
+            if (err){
+                context.done('error', 'error getting file' + err);
+            } else {
+                console.log('data:' + data);
+                var message = JSON.parse(data.Body);
+                console.log('message:' + message);
+                var eParams = {
+                        Destination: {
+                            ToAddresses: ["info@proudit.jp"]
+                        },
+                        Message: {
+                            Body: {
+                                Text: {
+                                    Data: "mail：" + message.mail+ "\n" + "subject："+message.title + "contents："+message.contents
+                                }
+                            },
+                            Subject: {
+                                Data: "ホームページから問い合わせがありました。"
+                            }
+                        },
+                        Source: "info@proudit.jp"
+                        };
+
+                        console.log('===SENDING EMAIL===');
+                        var email = ses.sendEmail(eParams, function(err, data){
+                            if(err){
+                                console.log("===EMAIL ERR===");
+                                console.log(err);
+                                context.done(null, 'ERR'); 
+                            }else {
+                                console.log("===EMAIL SENT===");
+                                console.log(data);
+                                context.done(null, 'SUCCESS');
+                            }
+                });
+                console.log("EMAIL CODE END");
+            }
+        }
+    );
+
+};
